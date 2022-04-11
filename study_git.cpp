70453943ad61074394167bb6ced0fe2d08dfb69c
Roman Fedorov <faktir@mail.ru>
2022-04-11 18:06:57 +0300

    add new file

---
+++ study_git.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+
+void hello() {
+    std::cout << "Hello" << std::endl;
+}
+
+void helloS(const std::string &name) {
+    std::cout << "Hello, " << name << "!" << std::endl;
+}
+
+int main()
+{
+    hello();
+    helloS("Roma");
+
+}
