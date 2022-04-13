0ef572df47aeaa5aab3ab779a966dcb08ce393a8
Roman Fedorov <faktir@mail.ru>
2022-04-13 18:34:49 +0300

    added void bye

--- study_git.cpp
+++ study_git.cpp
@@ -9,8 +9,13 @@ void helloS(const std::string &name) {
     std::cout << "Hello, " << name << "!" << std::endl;
 }
 
+void bye() {
+    std::cout << "Bye - bye" << std::endl;
+}
+
 int main()
 {
     hello();
+    bye();
 
 }
